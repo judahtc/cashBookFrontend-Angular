d3a58ca28a8c558fe548cf4fb891cbe48aa3f055
judahtc <jaycea247@gmail.com>
2024-01-10 21:51:36 +0200

    added initial responsiveness and a dump table

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
-  styleUrls: ['./portal.component.scss']
+  styleUrls: ['./portal.component.scss'],
 })
-export class PortalComponent {
+export class PortalComponent implements OnInit {
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+  sidebar = true;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    if (window.innerWidth <= 1024) {
+      this.sidebar = false;
+    } else {
+      this.sidebar = true;
+    }
+  }
+  toggle_sidebar() {
+    if (this.sidebar === true) {
+      this.sidebar = false;
+    } else {
+      this.sidebar = true;
+    }
+  }
 }
